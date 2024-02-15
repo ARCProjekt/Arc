e79ef719388cc7b55bf2a403a027b914e29ebca9
Wendesday36 <bertazoeanna@gmail.com>
2024-02-15 11:30:50 +0100

    kepek megjelenitesenek proba

--- frontend/src/KepLista.js
+++ frontend/src/KepLista.js
@@ -1,5 +1,4 @@
 export const  kepek = [
-    /* {nev : "Ember", src : "./galeriakepek/ember.jpg",leiras:"Ember a tengernel"}, */
     {nev : "Gepek", src : "./galeriakepek/havas.jpg",leiras:"havas út"},
     {nev : "Gepek", src : "./galeriakepek/havas.jpg",leiras:"havas út"},
     {nev : "Gepek", src : "./galeriakepek/havas.jpg",leiras:"havas út"},
