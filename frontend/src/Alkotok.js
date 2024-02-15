755ea843349d48f625ec2fcc6a564a84c11a113a
Wendesday36 <bertazoeanna@gmail.com>
2024-02-15 10:28:19 +0100

    Alkotok

---
+++ frontend/src/Alkotok.js
@@ -0,0 +1,11 @@
+export const alkotok =[
+    {nev : "Zöldlomb Legolas", src : "/kepek/legolas.jpg",szak:"designer"},
+    {nev : "Zöldlomb Legolas", src : "/kepek/legolas.jpg",szak:"designer"},
+    {nev : "Zöldlomb Legolas", src : "/kepek/legolas.jpg",szak:"designer"},
+    {nev : "Zöldlomb Legolas", src : "/kepek/legolas.jpg",szak:"designer"},
+    {nev : "Zöldlomb Legolas", src : "/kepek/legolas.jpg",szak:"designer"},
+    {nev : "Zöldlomb Legolas", src : "/kepek/legolas.jpg",szak:"designer"},
+    {nev : "Zöldlomb Legolas", src : "/kepek/legolas.jpg",szak:"designer"},
+    {nev : "Zöldlomb Legolas", src : "/kepek/legolas.jpg",szak:"designer"},
+
+]
