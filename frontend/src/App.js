30637d139670a52b6f43abfbe20ab862f768ef4e
Wendesday36 <bertazoeanna@gmail.com>
2024-02-15 10:47:02 +0100

    App bemasolva

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./LayOut";
+import Fooldal from "./pages/Fooldal";
+import Alkoto from "./pages/Alkoto";
+import Csapat from "./pages/Csapat";
+import Kategoria from "./pages/Kategoria";
+import Buszkesegeink from "./pages/Buszkesegeink";
+import Projekt from "./pages/Projekt";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Fooldal />} />
+          <Route path="alkoto" element={<Alkoto />} />
+          <Route path="csapat" element={<Csapat />} />
+          <Route path="kategoria" element={<Kategoria />} />
+          <Route path="buszkesegeink" element={<Buszkesegeink />} />
+          <Route path="projekt" element={<Projekt />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
