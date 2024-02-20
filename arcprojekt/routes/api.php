73fbd0239d54e347e4a128ecfb0ede34a12dbfc8
leilalino <aleila0921@gmail.com>
2024-02-20 09:50:03 +0100

    uj utvonalak

--- arcprojekt/routes/api.php
+++ arcprojekt/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AlkotoController;
+use App\Http\Controllers\GaleriaController;
+use App\Http\Controllers\KategoriaController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/api/alkotok', [AlkotoController::class, 'index']);
+/*csapat készítlése */
+/* /*csapat készítlése 
+Route::post('/api/csapatok/alkot', [CsapatController::class, 'alkot']);
+Route::post('/api/csapatok', [CsapatController::class, 'store']);
+Route::get('/api/csapatok', [CsapatController::class, 'index']);
+Route::get('/api/csapatok', [CsapatController::class, 'index']); */
+
+Route::middleware(['admin'])->group(function () {
+    Route::post('/userLetrehoz', [UserController::class, 'store']);
+});
+//alkoto letrehozasa
+Route::get('/alkotok/create', [AlkotoController::class, 'create']);
+Route::post('/alkotok', [AlkotoController::class, 'store']);
+Route::get('/alkotok', [AlkotoController::class, 'index']);
+Route::get('/kategoriaklista', [KategoriaController::class, 'kategoriakLista']);
+Route::get('/csapat_galeriaja/{csapat_id}', [GaleriaController::class, 'csapatGaleriaja']);
+//Route::get('/api/alkotok', [AlkotoController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
 });
