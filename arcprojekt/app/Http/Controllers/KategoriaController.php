0147dbe3ec194f616ae31ac7e94ac7a3cb7864ce
leilalino <aleila0921@gmail.com>
2024-02-20 10:13:06 +0100

    kategoriaLista javitasa

--- arcprojekt/app/Http/Controllers/KategoriaController.php
+++ arcprojekt/app/Http/Controllers/KategoriaController.php
@@ -4,10 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Kategoria;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class KategoriaController extends Controller
 {
-    public function kategoriakLista(){
-        return Kategoria::all();
+    public function kategoriakLista()
+    {
+      /*  $kategoriak = DB::table('kategoriak as k')
+            ->select('ny.magyar as nev')
+            ->join('nyelvs as ny', 'k.nyelv_id_elnevezes', '=', 'ny.nyelv_id')
+            ->get();
+        return $kategoriak;*/
+
+    $kategoriak = DB::select(
+        '
+        SELECT nyelvs.magyar
+        from kategorias 
+        inner join nyelvs
+        on kategorias.nyelv_id_elnevezes = nyelvs.nyelv_id
+        '
+    );
+    return response()->json(['kategoriak' => $kategoriak]);
     }
 }
