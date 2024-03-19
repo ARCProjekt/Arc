0c2ad6d70193d9d402bcee21bbaed4484153cd36
leilalino <aleila0921@gmail.com>
2024-03-19 15:25:41 +0100

    felhasznalo modositas bongeszoben is mukodik

--- arcprojekt/app/Http/Middleware/VerifyCsrfToken.php
+++ arcprojekt/app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         '/api/userletrehoz',
         '/api/alkotoletrehoz',
-        '/api/usertorol/*'
+        '/api/usertorol/*',
+        '/api/updateuser/*'
     ];
 }
