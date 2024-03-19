2f731556bcb133f750db307c7c8fd1598b2a55a9
Wendesday36 <bertazoeanna@gmail.com>
2024-03-19 17:52:25 +0100

    csapatok adatbazisbol kiolvasasa elkezdes

--- arcprojekt/app/Http/Controllers/CsapatController.php
+++ arcprojekt/app/Http/Controllers/CsapatController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Alkoto;
 use App\Models\Nyelv;
 use App\Models\Csapat;
@@ -10,16 +9,13 @@ use Illuminate\Support\Facades\DB;
 
 class CsapatController extends Controller
 {
-    public function csapatok()
-    {
+    public function csapatok(){
         return Csapat::all();
     }
-    public function csapatokKiir()
-    {
+    public function csapatokKiir(){
         $csapatok = DB::select('
 
         SELECT cs_azon, csapat_nev.magyar
-
         from csapats
         inner join nyelvs as csapat_nev
         on csapats.nyelv_id_csapat_nev = csapat_nev.nyelv_id
@@ -28,8 +24,20 @@ class CsapatController extends Controller
         ');
         return response()->json(['csapatok' => $csapatok]);
     }
-    public function show($id)
-    {
+    public function adottcsapatKiir(){
+        $csapatok = DB::select('
+
+        SELECT cs_azon, csapat_nev.magyar,alkotok.a_azon
+        from csapats
+        inner join nyelvs as csapat_nev
+        on csapats.nyelv_id_csapat_nev = csapat_nev.nyelv_id
+        inner join alkotos as alkotok
+        on csapat.cs_azon = alkotok.cs_azon
+
+        ');
+        return response()->json(['csapatok' => $csapatok]);
+    }
+    public function show($id){
         $csapat = DB::select('
         SELECT nyelvs.magyar as magyar_kategoria, csapat_nev.magyar, csapat_bemutat.magyar
         from csapats
@@ -47,13 +55,12 @@ class CsapatController extends Controller
         return response()->json(['adott_csapatt' => $csapat]);
     }
 
-    public function csapatGaleriaja($csapat_id)
-    {
+    public function csapatGaleriaja($csapat_id){
         $galery = DB::table('galerias as g')
-            ->select('*')
-            ->join('csapats as cs', 'g.galeria_id', '=', 'cs.galeria_id')
-            ->where('cs_azon', '=', $csapat_id)
-            ->get();
+        ->select('*')    
+        ->join('csapats as cs' ,'g.galeria_id','=','cs.galeria_id')
+        ->where('cs_azon', '=', $csapat_id)
+        ->get();
         return $galery;
     }
     public function create()
@@ -76,21 +83,21 @@ class CsapatController extends Controller
             'magyar_leiras' => 'required',
             'angol_leiras' => 'required',
         ]);
-
+    
         // Nyelv létrehozása magyar névvel
         $nyelvMagyarNev = Nyelv::create([
             'magyar' => $request->magyar_nev,
             'angol' => $request->angol_nev,
             'hol' => 'csapat nev',
         ]);
-
+    
         // Nyelv létrehozása magyar leírással
         $nyelvMagyarLeiras = Nyelv::create([
             'magyar' => $request->magyar_leiras,
             'angol' => $request->angol_leiras,
             'hol' => 'csapat leiras',
         ]);
-
+    
         // Csapat létrehozása
         $csapat = Csapat::create([
             'galeria_id' => $request->galeria_id,
@@ -98,11 +105,13 @@ class CsapatController extends Controller
             'nyelv_id_csapat_nev' => $nyelvMagyarNev->nyelv_id, // Megváltoztatva
             'nyelv_id_leiras' => $nyelvMagyarLeiras->nyelv_id, // Megváltoztatva
         ]);
-
+    
         // Az új csapat adatainak lekérése
         $createdCsapat = Csapat::with(['nyelvCsapatNev', 'nyelvLeiras', 'alkotok'])->find($csapat->id);
-
+    
         // Visszatérés az űrlap nézettel, például sikerüzenettel és alkotókkal
         return view('csapatok.create', ['message' => 'Csapat sikeresen létrehozva', 'alkotok' => Alkoto::all(), 'createdCsapat' => $createdCsapat]);
     }
+    
+    
 }
