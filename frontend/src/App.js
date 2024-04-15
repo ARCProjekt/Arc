2740185000da4e1b674abb16f0bec843040c1237
Wendesday36 <bertazoeanna@gmail.com>
2024-04-15 15:39:40 +0200

    csapat oldal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./LayOut";
 import Fooldal from "./pages/Fooldal";
 import Alkoto from "./pages/Alkoto";
-import Csapat from "./pages/Csapat";
 import Kategoria from "./pages/Kategoria";
 import Buszkesegeink from "./pages/Buszkesegeink";
 import Projekt from "./pages/Projekt";
@@ -28,7 +27,6 @@ function App() {
             <Route path="/" element={<Layout />}>
               <Route index element={<Fooldal />} />
               <Route path="alkoto" element={<Alkoto />} />
-              <Route path="csapat" element={<Csapat />} />
               <Route path="kategoria" element={<Kategoria />} />
               <Route path="buszkesegeink" element={<Buszkesegeink />} />
               <Route path="projekt" element={<Projekt />} />
